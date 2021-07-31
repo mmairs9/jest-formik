144d668559122e92ddcab42ad28cb0210c6f7626
Matthew Mairs <matthew.mairs@liberty-it.co.uk>
2021-07-31 10:26:59 +0100

    recreate issue with jest and formik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React from "react";
+
+import { Formik, Field } from "formik";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app">
+          <Formik
+              key="test"
+              enableReinitialize
+              onSubmit={values => {
+                console.log(values)
+              }}
+              initialValues={{
+                  repeats: 'daily'
+              }}
+          >
+              {(props) => {
+            /**
+             * Add your code here
+             */
+
+
+            return (
+                <form onSubmit={props.handleSubmit}>
+                    <>
+                        <Field
+                            {...props}
+                            >
+                            {({field}) => (
+                                <div className="field__wrapper">
+                                    <select {...field}  name="repeats" onBlur={props.handleBlur} onChange={props.handleChange}>
+                                           <option value="daily">Daily</option>
+                                        <option value="yearly">yearly</option>
+                                        <option value="minutes">minutes</option>
+                                        <option value="hourly">hourly</option>
+                                    </select>
+                                </div>
+                            )}
+                        </Field>
+                  <p>{new Date().toISOString()}</p>
+                        { console.log(new Date())}
+                  <input
+                      data-testid="the-input"
+                      value={props.values.name}
+                      type="text"
+                      onChange={props.handleChange}
+                      onBlur={props.handleSubmit}
+                  />
+                  <button type="submit">Submit</button>
+                    </>
+                </form>
+            )
+          }}
+        </Formik>
+      </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test the fake timer with formik', async () => {
+  jest.useFakeTimers("modern");
+  jest.setSystemTime(new Date("2023-02-18"));
+  render(<App/>);
+
+
+  //if we were to comment out this act block below then the test should pass - date seems to be resetting when the formik form is changed
+  await act(async () => {
+  const repeatsDropdown = (await waitFor(() => screen.getByText('Daily'))).parentElement
+  expect(repeatsDropdown).toBeTruthy()
+    userEvent.selectOptions(repeatsDropdown, 'yearly')
+  })
+
+
+  // :(
+  expect(new Date().getFullYear()).toEqual(2023)
 });
